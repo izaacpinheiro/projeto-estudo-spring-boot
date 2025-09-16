47129912ebda32d0e9e87b8e24045212f7d6fd0c
izaacpinheiro <izaacpinheiro.123@gmail.com>
2025-09-16 20:11:06 -0300

    feat: add repositories

---
+++ src/main/java/com/izaacpinheiro/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.izaacpinheiro.api.repositories;
+
+import com.izaacpinheiro.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
