098eef3eb980ff3ece28aec245215f015252e1a1
izaacpinheiro <izaacpinheiro.123@gmail.com>
2025-10-21 21:06:17 -0300

    fix: changing Data to Date

--- src/main/java/com/izaacpinheiro/api/domain/event/EventResponseDTO.java
+++ src/main/java/com/izaacpinheiro/api/domain/event/EventResponseDTO.java
@@ -1,7 +1,7 @@
 package com.izaacpinheiro.api.domain.event;
 
-import javax.xml.crypto.Data;
+import java.util.Date;
 import java.util.UUID;
 
-public record EventResponseDTO (UUID id, String title, String description, Data date, String city, String state, Boolean remote, String eventUrl) {
+public record EventResponseDTO (UUID id, String title, String description, Date date, String city, String state, Boolean remote, String eventUrl) {
 }
